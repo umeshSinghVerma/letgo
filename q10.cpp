cab40052aef09e964ebdd8ced0d578ed46170065
Umesh Singh Verma <119877487+umeshSinghVerma@users.noreply.github.com>
2024-11-17 17:15:02 +0530

    update q10

--- q10.cpp
+++ q10.cpp
@@ -1,71 +1,52 @@
-#include<bits/stdc++.h>
-using namespace std;
-#define int long long
-int l[200005],r[200005];
-signed main()
-{
-    cin.tie(0);
-    ios_base::sync_with_stdio(0);
+#include <bits/stdc++.h>
 
-    int t;
-    cin>>t;
+using i64 = long long;
+using u64 = unsigned long long;
+using u32 = unsigned;
+using u128 = unsigned __int128;
 
-    while(t--)
-    {
-        int n,k,total=0;
-        cin>>n>>k;
-        for(int i=0;i<n;i++) cin>>l[i];
-        for(int i=0;i<n;i++) cin>>r[i];
-        for(int i=0;i<n;i++)
-        {
-            total+=(r[i]-l[i]+1);
+void solve() {
+    int n;
+    std::cin >> n;
+    
+    std::vector<i64> a(n);
+    for (int i = 0; i < n; i++) {
+        std::cin >> a[i];
+    }
+    
+    i64 ans = 0;
+    if (n % 2 == 0) {
+        for (int i = 0; i < n; i += 2) {
+            ans = std::max(ans, a[i + 1] - a[i]);
         }
-        if(total<k) cout<<-1<<'\n';
-        else
-        {
-            int ans=0,tmp;
-            int now=0,cnt1=0,pos,tp;
-            for(int i=0;i<n;i++)
-            {
-                if(l[i]==r[i]) cnt1++;
-                now+=(r[i]-l[i]+1);
-                if(now>=k)
-                {
-                    pos=i;
-                    ans=2*(i+1)+r[i]-(now-k);
-                    tp=r[i]-(now-k);
-                    break;
+    } else {
+        ans = 1E18;
+        for (int i = 0; i < n; i += 2) {
+            i64 res = 0;
+            for (int j = 0; j < n; j += 2) {
+                if (j == i) {
+                    j--;
+                    continue;
                 }
+                res = std::max(res, a[j + 1] - a[j]);
             }
-            tmp=ans;
-            while(cnt1>0&&pos<n)
-            {
-                tmp-=2;
-                cnt1--;
-                if(tp+1<=r[pos])
-                {
-                    tmp++;
-                    tp++;
-                }
-                else if(pos+1==n)
-                {
-                    break;
-                }
-                else
-                {
-                    tmp+=2;
-                    tmp+=(l[pos+1]-r[pos]);
-                    tp=l[pos+1];
-                    pos++;
-                    if(l[pos]==r[pos])
-                    {
-                        cnt1++;
-                    }
-
-                }
-                ans=min(ans,tmp);
-            }
-            cout<<ans<<'\n';
+            ans = std::min(ans, res);
         }
     }
+    ans = std::max(ans, 1LL);
+    std::cout << ans << "\n";
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    
+    int t;
+    std::cin >> t;
+    
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
 }
